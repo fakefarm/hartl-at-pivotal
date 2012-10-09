b0d40c9d7e211be1f3a51957a725e408b3a0d5d7
Dave Woodall <dmwoodall@gmail.com>
2012-10-09 14:50:44 -0600

    life happend and I was not able to finish previous tutorial passes. So Hopefully this one will make it to chapter 11

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SamplePivotalApp::Application.config.secret_token = '27c952d5d4213fc2584eda686b020c8aefb497cc3e993dd245f0d1e1a2911f56739ce795dabab220ee6c3966a664713589b567371ad3595c828b7847d8ce59cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SamplePivotalApp::Application.config.session_store :cookie_store, key: '_samplePivotalApp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SamplePivotalApp::Application.config.session_store :active_record_store
