ebc1fd3d441f2f4583fd6e6ee987ee638aa70f12
Dave Woodall <dmwoodall@gmail.com>
2012-10-10 15:06:29 -0600

    finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 SamplePivotalApp::Application.routes.draw do
 
+  get "users/new"
+
   root to: 'static_pages#home'
 
+  match '/signup', to: 'users#new'
   match '/contact', to: 'static_pages#contact'
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
